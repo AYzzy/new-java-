28ae9b08bcb559e9f1cb3821c3331f2a23429080
ayomide <omoniyiayomide987@gmail.com>
2024-09-19 04:59:17 +0100

    commit

---
+++ src/PersonalPerson/Cat.java
@@ -0,0 +1,29 @@
+package PersonalPerson;
+
+public class Cat {
+    private String name;
+    private int age;
+    public Cat(String name,int age){
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void meow(){
+        System.out.println(name + " meow....");
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
--- src/PersonalPerson/classes.java
+++ src/PersonalPerson/classes.java
@@ -2,14 +2,14 @@ package PersonalPerson;
 
 public class classes {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.name="rose";
+        Cat cat = new Cat("Rose",2);
         cat.meow();
+        System.out.println(cat.getName());
+        System.out.println(cat.getAge());
+
+        Cat cat1 = new Cat("Kitten",3);
+        cat1.meow();
+        System.out.println(cat.getName());
     }
-    static class Cat {
-        String name;
-        void meow(){
-            System.out.println(name + " meow....");
-        }
-    }
+
 }
